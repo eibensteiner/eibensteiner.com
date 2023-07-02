4968da11afbf4b5a1adb71f5932e64da6b41cdcf
eibensteiner <me@dowu.xyz>
2023-07-02 12:24:34 +0200

    Analytics added

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     },
     css: ['@/assets/css/styles.css'],
     modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+    plugins: [{ src: '~/plugins/vercel.js', mode: 'client' }],
     image: {
         domains: ['s3.us-west-2.amazonaws.com']
     }
