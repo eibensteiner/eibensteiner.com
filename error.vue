f2502b6258057668a00b64ad023f44bea7db1048
eibensteiner <me@dowu.xyz>
2023-07-15 11:12:08 +0200

    Throw error instead of redirecting to 404

---
+++ error.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="sm:border-l sm:border-r border-gray-100 mx-auto sm:max-w-xl w-full">
+        <h1>{{ error.statusCode }}</h1>
+        <p>{{ error.message }}</p>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['error'])
+</script>
--- middleware/checkUser.js
+++ middleware/checkUser.js
@@ -3,7 +3,7 @@ import users from '~/plugins/users.js';
 export default function ({ params }) {
   // Perform your condition check
   if (!users.find(user => user.handle === params.user)) {
-    // Redirect to the 404 page
-    return navigateTo('/404');
+    // Throw 404 error
+    throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true });
   }
 }
