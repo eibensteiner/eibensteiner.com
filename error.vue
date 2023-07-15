2d09f975495d3402ef0e8bfb9820397391137d12
eibensteiner <me@dowu.xyz>
2023-07-15 11:43:27 +0200

    Error page redesigned

--- error.vue
+++ error.vue
@@ -1,9 +1,5 @@
 <template>
     <div class="mx-auto w-full h-screen flex flex-col items-center py-20">
-        <div
-            class="flex items-center justify-center w-9 h-9 outline outline-1 outline-gray-200 shadow-sm rounded-lg bg-gray-50 mb-4">
-            <span class="select-none">🚧</span>
-        </div>
         <h1 class="text-xl">{{ error.message }}</h1>
     </div>
 </template>
