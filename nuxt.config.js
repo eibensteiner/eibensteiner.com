1f815b30d445522cd3a637fa870e1a378a56d154
eibensteiner <treu_geschenke_0r@icloud.com>
2024-02-14 18:39:25 +0100

    Vercel Speed Insights added

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       htmlAttrs: { dir: 'ltr', lang: 'en' },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
+      ],
     },
     pageTransition: { name: 'page', mode: 'out-in' },
   },
